65ed7d5bef00202b1bbaf7bb0593336ea6299057
wploey <wploey@gmail.com>
2019-11-30 21:47:53 +0800

    Update install.sh

--- 16.04/install.sh
+++ 16.04/install.sh
@@ -53,7 +53,7 @@ function install_node_yarn {
 }
 
 function install_php {
-    apt-get install -y php7.4-bcmath php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-pgsql php7.4-readline php7.4-xml php7.4-zip php7.4-sqlite4 php7.4-redis
+    apt-get install -y php7.4-bcmath php7.4-pgsql php7.4-fpm libapache2-mod-php7.4 php-common php-geoip php-igbinary php-msgpack php-redis php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-gmp php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-soap php7.4-sqlite3 php7.4-xml php7.4-zip
 }
 
 function install_others {
